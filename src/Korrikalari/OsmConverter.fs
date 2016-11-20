e1ebdf692255f7462e5c7f28ef8dc92b11cad3e5
Asier <asierba@gmail.com>
2016-11-20 12:53:51 +0000

    Add namespaces. Change convert method to take XElement instead string.  Main program prints street names from a smaple osm file.

--- src/Korrikalari/OsmConverter.fs
+++ src/Korrikalari/OsmConverter.fs
@@ -1,30 +1,31 @@
-﻿module OsmConverter
+﻿namespace Korrikalari
 
-open System.Xml
-open System.Xml.Linq
+module OsmConverter =
 
-type Point = { lat: string; lon: string}
-type Street = { name: string; points: Point list}
+    open System.Xml
+    open System.Xml.Linq
 
-let xn name = XName.Get(name)
+    type Point = { lat: string; lon: string}
+    type Street = { name: string; points: Point list}
 
-let convert xml:seq<Street> = 
-    match xml with
-    | "" -> Seq.empty
-    | "<osm></osm>" -> Seq.empty
-    | _ -> 
+    let xn name = XName.Get(name)
 
-        let osmXmlElement = XElement.Parse(xml)
+    let convert (osmXmlElement:XElement):seq<Street> = 
+        let nodes = osmXmlElement.Elements(xn "node") |> Seq.toList
 
         let wayToStreet (wayXml:XElement)  =
-            let tag = wayXml.Elements(xn "tag")|> Seq.find(fun tag -> tag.Attribute(xn "v") <> null)
-            let streetName = tag.Attribute(xn "v").Value;
+            let nameTag = wayXml.Elements(xn "tag")|> Seq.find(fun tag -> tag.Attribute(xn "k").Value = "name")
+            let streetName = nameTag.Attribute(xn "v").Value;
 
             let nds = wayXml.Elements(xn "nd")
             let ndrefs = nds |> Seq.map(fun nd ->  nd.Attribute(xn "ref").Value)
-            let nodes = osmXmlElement.Elements(xn "node")
+            
             let matchingNodes = ndrefs |> Seq.map(fun ndref -> nodes |> Seq.find(fun node -> node.Attribute(xn "id").Value = ndref))
             let points = matchingNodes |> Seq.map(fun node -> {lat= node.Attribute(xn "lat").Value; lon=node.Attribute(xn "lon").Value}) |> Seq.toList
+
             {name = streetName; points = points}
 
-        osmXmlElement.Elements(xn "way") |> Seq.map wayToStreet
+        let wayHasName (wayXml:XElement) =
+            wayXml.Elements(xn "tag")|> Seq.exists(fun tag -> tag.Attribute(xn "k").Value = "name")
+
+        osmXmlElement.Elements(xn "way") |> Seq.filter wayHasName |> Seq.map wayToStreet
--- tests/Korrikalari.Tests/OsmConvertionTests.fs
+++ tests/Korrikalari.Tests/OsmConvertionTests.fs
@@ -1,77 +1,96 @@
-﻿module OsmConvertionTests
+﻿namespace Korrikalari.Tests
 
-open Xunit
-open OsmConverter
+module OsmConvertionTests =
 
-[<Theory>]
-[<InlineData("")>]
-[<InlineData("<osm></osm>")>]
-let ``no content returns nothing`` osmXml =
-    Assert.Equal<Street seq>(convert osmXml, Seq.empty)
+    open Xunit
+    open Korrikalari.OsmConverter
+    open System.Xml.Linq
 
-[<Fact>]
-let ``converts a way to a street``() =
-    let osmXml = """
-    <osm>
-     <way>
-      <tag v="Nowhere Street"/>
-     </way>
-    </osm>"""
-    let streets = [{ name = "Nowhere Street"; points = List.empty }]
+    let createXml xml =
+        XElement.Parse(xml)
 
-    let result = convert osmXml |> Seq.toList
+    [<Fact>]
+    let ``no content returns no streets`` =
+        let osmXml = createXml "<osm></osm>"
+        Assert.Empty(convert osmXml)
 
-    Assert.Equal<Street list>(result, streets)
+    [<Fact>]
+    let ``converts a way to a street``() =
+        let osmXml = createXml """
+        <osm>
+            <way>
+                <tag k="highway" v="residential"/>
+                <tag k="name" v="Nowhere Street"/>
+            </way>
+        </osm>"""
+        let streets = [{ name = "Nowhere Street"; points = List.empty }]
 
-[<Fact>]
-let ``converts a way with points to a street``() =
-    let osmXml = """
-    <osm>
-        <node id="292403538" lat="54.0901447" lon="12.2516513"/>
-        <node id="298884289" lat="54.0901447" lon="13.2516513"/>
-        <node id="261728686" lat="54.0901447" lon="14.2516513"/>
+        let result = convert osmXml |> Seq.toList
+
+        Assert.Equal<Street list>(result, streets)
+
+    [<Fact>]
+    let ``ignore ways with no name``() =
+        let osmXml = createXml """
+        <osm>
+            <way>
+                <tag k="highway" v="residential"/>
+            </way>
+        </osm>"""
+        let streets = []
+
+        Assert.Empty(convert osmXml)
+
+    [<Fact>]
+    let ``converts a way with points to a street``() =
+        let osmXml = createXml """
+        <osm>
+            <node id="292403538" lat="54.0901447" lon="12.2516513"/>
+            <node id="298884289" lat="54.0901447" lon="13.2516513"/>
+            <node id="261728686" lat="54.0901447" lon="14.2516513"/>
+            <way>
+                <nd ref="292403538"/>
+                <nd ref="298884289"/>
+                <nd ref="261728686"/>
+                <tag k="name" v="Nowhere Street"/>
+            </way>
+        </osm>"""
+
+        let result = convert osmXml |> Seq.toList
+
+        let streets = [{ name = "Nowhere Street"; 
+                        points = [{ lat= "54.0901447"; lon= "12.2516513" }; { lat= "54.0901447"; lon= "13.2516513" }; { lat= "54.0901447"; lon= "14.2516513" }]
+                       }]
+        Assert.Equal<Street list>(result, streets)
+
+    [<Fact>]
+    let ``converts multiple ways to streets``() =
+        let osmXml = XElement.Parse("""<osm>
+        <node id="1" lat="54" lon="12"/>
+        <node id="2" lat="54" lon="13"/>
+        <node id="3" lat="54" lon="14"/>
+        <node id="4" lat="56" lon="23"/>
+        <node id="5" lat="57" lon="43"/>
         <way>
-            <nd ref="292403538"/>
-            <nd ref="298884289"/>
-            <nd ref="261728686"/>
-            <tag v="Nowhere Street"/>
+        <nd ref="1"/>
+        <nd ref="2"/>
+        <nd ref="3"/>
+        <tag k="highway" />
+        <tag k="name" v="Street 1"/>
         </way>
-    </osm>"""
-    let points = [{ lat= "54.0901447"; lon= "12.2516513" }; { lat= "54.0901447"; lon= "13.2516513" }; { lat= "54.0901447"; lon= "14.2516513" }]
-    let streets = [{ name = "Nowhere Street";points = points}]
+        <way>
+        <nd ref="3"/>
+        <nd ref="4"/>
+        <nd ref="5"/>
+        <tag k="highway" />
+        <tag k="name" v="Street 2"/>
+        </way>
+        </osm>""")
+        
+    
+        let result = convert osmXml |> Seq.toList
 
-    let result = convert osmXml |> Seq.toList
-
-    Assert.Equal<Street list>(result, streets)
-
-[<Fact>]
-let ``converts multiple ways to streets``() =
-    let osmXml = """<osm>
-     <node id="1" lat="54" lon="12"/>
-     <node id="2" lat="54" lon="13"/>
-     <node id="3" lat="54" lon="14"/>
-     <node id="4" lat="56" lon="23"/>
-     <node id="5" lat="57" lon="43"/>
-     <way>
-      <nd ref="1"/>
-      <nd ref="2"/>
-      <nd ref="3"/>
-      <tag k="highway" />
-      <tag k="name" v="Street 1"/>
-     </way>
-     <way>
-      <nd ref="3"/>
-      <nd ref="4"/>
-      <nd ref="5"/>
-      <tag k="highway" />
-      <tag k="name" v="Street 2"/>
-     </way>
-    </osm>"""
-    let streets = [
-    { name = "Street 1"; points = [{ lat= "54"; lon= "12" }; { lat= "54"; lon= "13" }; { lat= "54"; lon= "14" }]};
-    { name = "Street 2"; points = [{ lat= "54"; lon= "14" }; { lat= "56"; lon= "23" }; { lat= "57"; lon= "43" }]}
-    ]
- 
-    let result = convert osmXml |> Seq.toList
-
-    Assert.Equal<Street list>(result, streets)
+        let streets = [ 
+            { name = "Street 1"; points = [{ lat= "54"; lon= "12" }; { lat= "54"; lon= "13" }; { lat= "54"; lon= "14" }]}; 
+            { name = "Street 2"; points = [{ lat= "54"; lon= "14" }; { lat= "56"; lon= "23" }; { lat= "57"; lon= "43" }]}]
+        Assert.Equal<Street list>(result, streets)
