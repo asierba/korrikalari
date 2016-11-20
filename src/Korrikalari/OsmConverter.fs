0a71e20e8066f9501e73134669d428210dca92f6
Asier <1537266+asierba@users.noreply.github.com>
2016-11-20 15:35:01 +0000

    Improver performance OSM reading.

--- src/Korrikalari/OsmConverter.fs
+++ src/Korrikalari/OsmConverter.fs
@@ -5,13 +5,16 @@ module OsmConverter =
     open System.Xml
     open System.Xml.Linq
 
-    type Point = { lat: string; lon: string}
+    type Point = { lat: string; lon: string; id:string}
     type Street = { name: string; points: Point list}
 
     let xn name = XName.Get(name)
 
+    let toPoint (nodeXml:XElement) =
+        { lat= nodeXml.Attribute(xn "lat").Value; lon=nodeXml.Attribute(xn "lon").Value;id=nodeXml.Attribute(xn "id").Value}
+
     let convert (osmXmlElement:XElement):seq<Street> = 
-        let nodes = osmXmlElement.Elements(xn "node") |> Seq.toList
+        let nodes = osmXmlElement.Elements(xn "node") |> Seq.map toPoint |> Seq.toList
 
         let wayToStreet (wayXml:XElement)  =
             let nameTag = wayXml.Elements(xn "tag")|> Seq.find(fun tag -> tag.Attribute(xn "k").Value = "name")
@@ -19,9 +22,8 @@ module OsmConverter =
 
             let nds = wayXml.Elements(xn "nd")
             let ndrefs = nds |> Seq.map(fun nd ->  nd.Attribute(xn "ref").Value)
-            
-            let matchingNodes = ndrefs |> Seq.map(fun ndref -> nodes |> Seq.find(fun node -> node.Attribute(xn "id").Value = ndref))
-            let points = matchingNodes |> Seq.map(fun node -> {lat= node.Attribute(xn "lat").Value; lon=node.Attribute(xn "lon").Value}) |> Seq.toList
+
+            let points = ndrefs |> Seq.map(fun ndref -> nodes |> Seq.find(fun node -> node.id = ndref)) |> Seq.toList
 
             {name = streetName; points = points}
 
