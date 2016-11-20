f0ecab3b1f5f5fb18d61721753c2bd27f08f5e61
Asier <1537266+asierba@users.noreply.github.com>
2016-11-20 12:53:51 +0000

    Add namespaces. Change convert method to take XElement instead string.  Main program prints street names from a smaple osm file.

--- src/Korrikalari/Program.fs
+++ src/Korrikalari/Program.fs
@@ -1,6 +1,29 @@
 ﻿open System
+open System.Xml.Linq
+open Korrikalari.OsmConverter
 
 [<EntryPoint>]
 let main argv = 
-    printfn "Hello World!"
+    let fileName = "OsmSamples/test.osm"
+
+    printfn "Loading OSM File..."
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let osmXml = XElement.Load(fileName)
+    stopWatch.Stop()
+    printfn "Time: %f s" stopWatch.Elapsed.TotalSeconds 
+    
+    printfn "Mapping XML Data to streets..."    
+    let stopWatch = System.Diagnostics.Stopwatch.StartNew()
+    let streets = convert osmXml |> Seq.toList
+    stopWatch.Stop()
+    printfn "Time: %f s" stopWatch.Elapsed.TotalSeconds
+
+    let printStreetName street =
+        printfn "%s" street.name
+
+    printfn "Streets in OSM File:"
+    streets |> Seq.iter printStreetName
+
+    streets  |> List.length |> printfn "%i streets in file!"
+
     0 
