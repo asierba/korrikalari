5f918b5d5bace06baf07599572027f74647c6b91
Asier <asierba@gmail.com>
2016-11-21 07:56:43 +0000

    Split Osm Converter into small methods

--- src/Korrikalari/OsmConverter.fs
+++ src/Korrikalari/OsmConverter.fs
@@ -11,23 +11,30 @@ module OsmConverter =
     let xn name = XName.Get(name)
 
     let toPoint (nodeXml:XElement) =
-        { lat= nodeXml.Attribute(xn "lat").Value; lon=nodeXml.Attribute(xn "lon").Value;id=nodeXml.Attribute(xn "id").Value}
+        { lat = nodeXml.Attribute(xn "lat").Value; lon = nodeXml.Attribute(xn "lon").Value; id = nodeXml.Attribute(xn "id").Value}
+
+    let getStreetName (wayXml:XElement) =
+        let nameTag = wayXml.Elements(xn "tag")|> Seq.find(fun tag -> tag.Attribute(xn "k").Value = "name")
+        nameTag.Attribute(xn "v").Value;
+
+    let ndToPoint nodes (ndXml:XElement) =
+        let ndref = ndXml.Attribute(xn "ref").Value
+        nodes |> Seq.find(fun node -> node.id = ndref)
+
+    let getPoints nodes (wayXml:XElement) =
+        let nds = wayXml.Elements(xn "nd")
+        nds |> Seq.map (ndToPoint nodes) |> Seq.toList
+
+    let wayToStreet nodes (wayXml:XElement)   =
+            let streetName = getStreetName wayXml
+            let points = getPoints nodes wayXml
+            { name = streetName; points = points}
+            
+    let wayHasName (wayXml:XElement) =
+            wayXml.Elements(xn "tag")|> Seq.exists(fun tag -> tag.Attribute(xn "k").Value = "name")
 
     let convert (osmXmlElement:XElement):seq<Street> = 
         let nodes = osmXmlElement.Elements(xn "node") |> Seq.map toPoint |> Seq.toList
+        let waysWithName = osmXmlElement.Elements(xn "way") |> Seq.filter wayHasName
 
-        let wayToStreet (wayXml:XElement)  =
-            let nameTag = wayXml.Elements(xn "tag")|> Seq.find(fun tag -> tag.Attribute(xn "k").Value = "name")
-            let streetName = nameTag.Attribute(xn "v").Value;
-
-            let nds = wayXml.Elements(xn "nd")
-            let ndrefs = nds |> Seq.map(fun nd ->  nd.Attribute(xn "ref").Value)
-
-            let points = ndrefs |> Seq.map(fun ndref -> nodes |> Seq.find(fun node -> node.id = ndref)) |> Seq.toList
-
-            {name = streetName; points = points}
-
-        let wayHasName (wayXml:XElement) =
-            wayXml.Elements(xn "tag")|> Seq.exists(fun tag -> tag.Attribute(xn "k").Value = "name")
-
-        osmXmlElement.Elements(xn "way") |> Seq.filter wayHasName |> Seq.map wayToStreet
+        waysWithName |> Seq.map (wayToStreet nodes)
