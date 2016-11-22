11d5e7baaa127149075906a86c1a1679f4b746cf
Asier <1537266+asierba@users.noreply.github.com>
2016-11-22 07:52:20 +0000

    Add a function get points from strava endpoint

--- src/Korrikalari/Program.fs
+++ src/Korrikalari/Program.fs
@@ -2,8 +2,37 @@
 open System.Xml.Linq
 open Korrikalari.OsmConverter
 
+open Newtonsoft.Json
+
+open System
+open System.IO
+open System.Net.Http
+open Newtonsoft.Json.Linq
+ 
+let getActivityCoordinates activityId =
+    let url = sprintf "https://www.strava.com/api/v3/activities/%i/streams/latlng" activityId
+    use client = new HttpClient()
+
+    client.DefaultRequestHeaders.Add("Authorization", "Bearer X")
+
+    let task = client.GetAsync(url)
+    let result = task.Result
+    let content = result.Content.ReadAsStringAsync().Result
+    let jarray = JArray.Parse(content)
+    let first = (jarray |> Seq.toList).Head
+    let data = first.Value<JArray>("data")
+    let toCoordinate item =
+        let coordinates = item |> Seq.toList |> Seq.map(float) |> Seq.toList
+        (coordinates.[0], coordinates.[1])
+    data |> Seq.map(toCoordinate)
+
 [<EntryPoint>]
 let main argv = 
+    let coordinates = getActivityCoordinates 780826706
+
+    coordinates |> Seq.iter (printfn "%A")
+
+   
     let fileName = "OsmSamples/test.osm"
 
     printfn "Loading OSM File..."
