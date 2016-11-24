692221ca5b6556af7e2f99177d1ce0cfb84b7511
Asier <1537266+asierba@users.noreply.github.com>
2016-11-24 22:43:14 +0000

    Code moved from Program.fs to new modules

---
+++ src/Korrikalari/StravaClient.fs
@@ -0,0 +1,28 @@
+namespace Korrikalari
+
+module StravaClient = 
+
+    open System
+    open System.IO
+    open System.Net.Http
+    open Newtonsoft.Json.Linq
+
+    let getToken =
+        System.IO.File.ReadLines("strava.token") |> Seq.head
+
+    let getActivityCoordinates activityId =
+        let url = sprintf "https://www.strava.com/api/v3/activities/%i/streams/latlng" activityId
+        use client = new HttpClient()
+
+        client.DefaultRequestHeaders.Add("Authorization", sprintf "Bearer %s" getToken)
+
+        let task = client.GetAsync(url)
+        let result = task.Result
+        let content = result.Content.ReadAsStringAsync().Result
+        let jarray = JArray.Parse(content)
+        let first = (jarray |> Seq.toList).Head
+        let data = first.Value<JArray>("data")
+        let toCoordinate item =
+            let coordinates = item |> Seq.map(float) |> Seq.toList
+            (coordinates.[0], coordinates.[1])
+        data |> Seq.map(toCoordinate)
