12ef9cf3f58e5401812aef88f123035782117fe1
Asier <asierba@gmail.com>
2016-12-13 07:01:58 +0000

    Get street names from activity coordinates using google maps api

--- src/Korrikalari/Program.fs
+++ src/Korrikalari/Program.fs
@@ -10,14 +10,16 @@ let measure action =
 
 [<EntryPoint>]
 let main argv = 
-    let streets = StreetRepository.getStreets |> Seq.sortBy(fun street -> street.name)
-    let coordinates = StravaClient.getActivityCoordinates 780826706
-
-    // coordinates |> Seq.iter (printfn "%A")
-
+    // let streets = StreetRepository.getStreets |> Seq.sortBy(fun street -> street.name)
     // let printStreet (street:OsmConverter.Street) =
     //     printfn "%s" street.name
     //     street.points |> Seq.iter (fun point -> printfn "\t(%s, %s)" point.lat point.lon)
     // streets |> Seq.iter (printStreet)
     // streets  |> Seq.length |> printfn "%i streets in file!"
+
+    let coordinates = StravaClient.getActivityCoordinates 780826706
+    // coordinates |> Seq.iter (printfn "%A")
+
+    let streetNames = coordinates |> Seq.map  GoogleMapsClient.getClosestStreet |> Seq.choose id |> Seq.distinct
+    streetNames |> Seq.iter (printfn "%A")
     0 
