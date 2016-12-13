ffa274d84fb508a788469d38dbff96f04c81642b
Asier <1537266+asierba@users.noreply.github.com>
2016-12-13 07:29:10 +0000

    Get coordinates from most recent activity in strava

--- src/Korrikalari/StravaClient.fs
+++ src/Korrikalari/StravaClient.fs
@@ -7,11 +7,13 @@ module StravaClient =
     open System.Net.Http
     open Newtonsoft.Json.Linq
 
+    type Activity = { id:int }
+
     let getToken =
         System.IO.File.ReadLines("strava.token") |> Seq.head
 
-    let getActivityCoordinates activityId =
-        let url = sprintf "https://www.strava.com/api/v3/activities/%i/streams/latlng" activityId
+    let getActivityCoordinates activity =
+        let url = sprintf "https://www.strava.com/api/v3/activities/%i/streams/latlng" activity.id
         use client = new HttpClient()
 
         client.DefaultRequestHeaders.Add("Authorization", sprintf "Bearer %s" getToken)
@@ -26,3 +28,16 @@ module StravaClient =
             let coordinates = item |> Seq.map(float)
             { lat = Seq.item 0 coordinates; lon= Seq.item 1 coordinates}
         data |> Seq.map(toCoordinate)
+
+    let getMostRecentActivity =
+        let url = "https://www.strava.com/api/v3/activities?per_page=1" 
+        use client = new HttpClient()
+
+        client.DefaultRequestHeaders.Add("Authorization", sprintf "Bearer %s" getToken)
+
+        let task = client.GetAsync(url)
+        let result = task.Result
+        let content = result.Content.ReadAsStringAsync().Result
+        let jarray = JArray.Parse(content)
+        let first = (jarray |> Seq.toList).Head
+        { id = first.Value<int>("id") }
