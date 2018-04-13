24cc59dd52ba6b9d05da5fa53efee42cf033c669
Asier <asierba@gmail.com>
2018-04-13 14:59:06 +0100

    Create deployment script to run through traveis

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 rm -rf deployment
 cd src/Korrikalari
 dotnet publish -o ../../deployment
+cd ../..
+docker build -t korrikalari .
+docker tag korrikalari asierba/korrikalari
+docker push asierba/korrikalari
