3dde8638dde15421deed7a40ce4d43f15a72280e
Asier <1537266+asierba@users.noreply.github.com>
2018-04-13 15:49:08 +0100

    add docker login to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ dotnet publish -o ../../deployment
 cd ../..
 docker build -t korrikalari .
 docker tag korrikalari asierba/korrikalari
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker push asierba/korrikalari
