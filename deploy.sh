fb2a76db479d78f8b577757c15de6ebcea269110
Asier <1537266+asierba@users.noreply.github.com>
2018-04-13 16:47:29 +0100

    convert scripts to make file targets

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-rm -rf deployment
-cd src/Korrikalari
-dotnet publish -o ../../deployment
-cd ../..
-docker build -t korrikalari .
-docker tag korrikalari asierba/korrikalari
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker push asierba/korrikalari
