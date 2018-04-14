2ee9676cae253656b3ba0e87d697fcdfce4347fe
Asier <asierba@gmail.com>
2018-04-14 09:50:15 +0100

    Doesn't show any street for an indoor activy

--- tests/Korrikalari.Tests/StravaClientTests.fs
+++ tests/Korrikalari.Tests/StravaClientTests.fs
@@ -19,7 +19,7 @@ module StravaClientTests =
 
     [<Fact>]
     let ``get cooordinates for an activity``() =
-        let httpGet url =
+        let httpGet _ =
             """
            [
                 {
@@ -38,5 +38,27 @@ module StravaClientTests =
             {lat= 51.498765; lon= -0.141829 };
             {lat= 51.49027; lon= -0.150}
         ] 
+        Assert.Equal<seq<Coordinate>>(expected, Seq.toList coords)
+
+    [<Fact>]
+    let ``get no cooordinates for an indoor activity``() =
+        let httpGet _ =
+            """
+           [
+              {
+                "type": "distance",
+                "data": [
+                  1.5,
+                  22.8      
+                ],
+                "series_type": "distance",
+                "original_size": 253,
+                "resolution": "high"
+              }
+            ]
+            """
+        let coords = getActivityCoordinates httpGet { id = 806422995}
+
+        let expected = [] 
         Assert.Equal<seq<Coordinate>>(expected, Seq.toList coords)
 
